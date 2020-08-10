5e601f491372a6be54f49326fead3aba2a4fc216
Devsolution <q3construction1@gmail.com>
2020-08-10 19:21:46 +0800

    Update i18n

--- resources/js/src/plugins/i18n.js
+++ resources/js/src/plugins/i18n.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-import enMessages from '../lang/en.json'
-import esMessages from '../lang/es.json'
-import ptMessages from '../lang/pt.json'
+import enMessages from '../lang/en'
+import esMessages from '../lang/es'
+import ptMessages from '../lang/pt'
 
 Vue.use(VueI18n)
 
-const messages = {
+const langMessages = {
 	'en': enMessages,
 	'es': esMessages,
 	'pt': ptMessages
@@ -16,7 +16,7 @@ const messages = {
 const i18n = new VueI18n({
 	locale: 'en', // set locale
 	fallbackLocale: ['en', 'es', 'pt'], // set fallback locale
-	messages, // set locale messages
+	langMessages, // set locale messages
 })
 
 export default i18n
