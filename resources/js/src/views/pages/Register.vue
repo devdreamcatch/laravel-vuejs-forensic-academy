0590f4fe4cc6428dfb29456c68dc3dc60ebbceb0
Devsolution <q3construction1@gmail.com>
2020-08-11 19:34:31 +0800

    Draft register

--- resources/js/src/views/pages/Register.vue
+++ resources/js/src/views/pages/Register.vue
@@ -15,7 +15,7 @@
 				<div slot="no-body" class="full-page-bg-color">
 					<div class="vx-row no-gutter">
 						<div class="vx-col sm:w-full md:w-full lg:w-full mx-auto self-center  d-theme-dark-bg">
-							<div class="px-8 pt-8">
+							<div class="px-8 p-8">
 								<div class="vx-col hidden sm:hidden md:hidden lg:block lg:w-1/2 mx-auto self-center">
 									<img class="mx-auto" src="@assets/images/pages/logo.png" alt="branding logo">
 								</div>
@@ -23,39 +23,152 @@
 									<h4 class="mb-4">{{ $t('register.create') }}</h4>
 								</div>
 								<vs-alert active="true" color="danger">
-									{{ $t('register.fill') }} <b>{{ $t('register.invices') }}</b> {{ $t('register.and') }}<b>{{ $t('register.certificaters') }}</b>.
+									{{ $t('register.fill') }} <b>{{ $t('register.invoices') }}</b> {{ $t('register.and') }} <b>{{ $t('register.certificaters') }}</b>.
 								</vs-alert>
-								<div class="clearfix mt-2">
-									<vs-row vs-w="12">
-										<vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="4" vs-sm="12" vs-xs="12" >
+								<div class="clearfix mt-4">
+									<div class="vx-row mb-2">
+										<div class="vx-col sm:w-4/12 w-full">
 											<vs-input
-												v-bind:label-placeholder="{{ $t('') }}"
-												name="displayName"
-												placeholder="Name"
-												class="w-full" />
-											<span class="text-danger text-sm"></span>
-										</vs-col>
-										<vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="5" vs-sm="12" vs-xs="12" >
+												class="w-full"
+												name="name"
+												v-model="name"
+												v-bind:label="$t('global.field.Name')"
+												v-bind:placeholder="$t('global.field.Name')"
+												v-validate="'required|alpha'"
+                    						data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('name') }}</span>
+										</div>
+										<div class="vx-col sm:w-5/12 w-full">
 											<vs-input
-												label-placeholder="Name"
-												name="displayName"
-												placeholder="Name"
-												class="w-full" />
-											<span class="text-danger text-sm"></span>
-										</vs-col>
-										<vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="3" vs-sm="12" vs-xs="12" >
+												class="w-full"
+												name="surname"
+												v-model="surname"
+												v-bind:label="$t('global.field.Surname')"
+												v-bind:placeholder="$t('global.field.Surname')"
+												v-validate="'required|alpha'"
+                    						data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('surname') }}</span>
+										</div>
+										<div class="vx-col sm:w-3/12 w-full">
 											<vs-input
-												label-placeholder="Name"
-												name="displayName"
-												placeholder="Name"
-												class="w-full" />
-											<span class="text-danger text-sm"></span>
-										</vs-col>
-									</vs-row>
+												class="w-full"
+												name="cpf"
+												v-model="cpf"
+												v-bind:label="$t('global.field.CPF')"
+												v-bind:placeholder="$t('global.field.CPF')"
+												v-validate="'required'"
+												v-mask="'###.###.###-##'"
+												data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('cpf') }}</span>
+										</div>
+									</div>
 
-									<vs-checkbox class="mt-6">I accept the terms & conditions.</vs-checkbox>
-									<vs-button  type="border" to="/login" class="mt-6">Login</vs-button>
-									<vs-button class="float-right mt-6">Register</vs-button>
+									<div class="vx-row mb-2">
+										<div class="vx-col sm:w-1/4 w-full">
+											<label class="vs-input--label">{{ $t('global.field.Sex') }}</label>
+											<v-select
+												name="sex"
+												v-model="sex"
+												:options="sexOptions"
+												:clearable="false"
+												v-validate="'required'"
+												data-vv-validate-on="blur"
+												:dir="$vs.rtl ? 'rtl' : 'ltr'" />
+											<span class="text-danger text-sm">{{ errors.first('sex') }}</span>
+										</div>
+										<div class="vx-col sm:w-1/4 w-full">
+											<label class="vs-input--label">{{ $t('global.field.DateOfBirth') }}</label>
+      									<flat-pickr name="date_of_birth"
+												class="w-full"
+												v-model="dateOfBirth"
+												:config="{ dateFormat: 'Y-m-d' }" />
+											<span class="text-danger text-sm">{{ errors.first('DateOfBirth') }}</span>
+										</div>
+										<div class="vx-col sm:w-1/4 w-full">
+											<vs-input
+												class="w-full"
+												name="telephone"
+												v-model="telephone"
+												v-bind:label="$t('global.field.Telephone')"
+												v-bind:placeholder="$t('global.field.Telephone')"
+												v-validate="'required'"
+												data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('telephone') }}</span>
+										</div>
+										<div class="vx-col sm:w-1/4 w-full">
+											<vs-input
+												class="w-full"
+												name="whatsapp"
+												v-model="whatsapp"
+												v-bind:label="$t('global.field.Whatsapp')"
+												v-bind:placeholder="$t('global.field.Whatsapp')"
+												v-validate="'required'"
+												data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('whatsapp') }}</span>
+										</div>
+									</div>
+
+									<div class="vx-row mb-2">
+										<div class="vx-col sm:w-1/2 w-full">
+											<vs-input
+												class="w-full"
+												name="email"
+												v-model="email"
+												v-bind:label="$t('global.field.Email')"
+												v-bind:placeholder="$t('global.field.Email')"
+												v-validate="'required|email'"
+												ref="email"
+												data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('email') }}</span>
+										</div>
+										<div class="vx-col sm:w-1/2 w-full">
+											<vs-input
+												class="w-full"
+												name="email_confirmation"
+												v-model="emailConfirmation"
+												v-bind:label="$t('global.field.EmailConfirmation')"
+												v-bind:placeholder="$t('global.field.EmailConfirmation')"
+												v-validate="'required|email|confirmed:email'"
+												data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('email_confirmation') }}</span>
+										</div>
+									</div>
+
+									<div class="vx-row mb-2">
+										<div class="vx-col sm:w-1/2 w-full">
+											<vs-input
+												class="w-full"
+												name="password"
+                    						type="password"
+												v-model="password"
+												v-bind:label="$t('global.field.CreatePassword')"
+												v-bind:placeholder="$t('global.field.CreatePassword')"
+												v-validate="'required'"
+												ref="password"
+												data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('password') }}</span>
+										</div>
+										<div class="vx-col sm:w-1/2 w-full">
+											<vs-input
+												class="w-full"
+												name="password_confirmation"
+												type="password"
+												v-model="passwordConfirmation"
+												v-bind:label="$t('global.field.PasswordConfirmation')"
+												v-bind:placeholder="$t('global.field.PasswordConfirmation')"
+												v-validate="'required|confirmed:password'"
+												data-vv-validate-on="blur" />
+											<span class="text-danger text-sm">{{ errors.first('password_confirmation') }}</span>
+										</div>
+									</div>
+
+									<vs-checkbox class="mt-6" v-model="isTermsConditionAccepted">{{ $t('register.acceptCondition') }}</vs-checkbox>
+									<vs-button  type="border" to="/login" class="mt-6">{{ $t('global.field.Login') }}</vs-button>
+									<vs-button
+										class="float-right mt-6"
+										@click="register" :disabled="!validateForm">
+										{{ $t('global.field.Register') }}
+									</vs-button>
 								</div>
 							</div>
 						</div>
@@ -67,12 +180,99 @@
 </template>
 
 <script>
-export default {
+import vSelect from 'vue-select'
+import flatPickr from 'vue-flatpickr-component'
+import 'flatpickr/dist/flatpickr.css'
 
+export default {
+	data () {
+    	return {
+			name: "",
+			surname: "",
+			cpf: "",
+			sexOptions: [
+				{id: 1, label: this.$t('global.field.Male')},
+				{id: 2, label: this.$t('global.field.Female')},
+				{id: 3, label: this.$t('global.field.Other')}
+      	],
+			sex: {id: 1, label: this.$t('global.field.Male')},
+			dateOfBirth: '',
+			telephone: '',
+			whatsapp: '',
+			email: '',
+			emailConfirmation: '',
+			password: '',
+			passwordConfirmation: '',
+			isTermsConditionAccepted: true
+    }
+  	},
+
+	computed: {
+		validateForm () {
+			return ! this.isTermsConditionAccepted === false && !this.errors.any() && this.dateOfBirth !== ''
+		}
+	},
+
+	methods: {
+		checkLogin () {
+			// If user is already logged in notify
+			if (this.$store.state.auth.isUserLoggedIn()) {
+				// Close animation if passed as payload
+				// this.$vs.loading.close()
+
+				this.$vs.notify({
+					title: this.$t('login.attempt_title'),
+					text: this.$t('login.already_login'),
+					iconPack: 'feather',
+					icon: 'icon-alert-circle',
+					color: 'warning'
+				})
+				return false
+			}
+			return true
+		},
+
+		register () {
+			// If form is not validated or user is already login return
+			if (!this.validateForm || !this.checkLogin()) return
+			this.$vs.loading()
+			const payload = {
+				userDetails: {
+					name: this.name,
+					surname: this.surname,
+					cpf: this.cpf,
+					sex: this.sex.id,
+					dateOfBirth: this.dateOfBirth,
+					telephone: this.telephone,
+					whatsapp: this.whatsapp,
+					email: this.email,
+					password: this.password,
+				}
+			}
+			this.$store.dispatch('auth/registerUserJWT', payload)
+				.then(() => { this.$vs.loading.close() })
+				.catch(error => {
+					this.$vs.loading.close()
+					if (error.response.status === 422) {
+						this.$vs.notify({
+							title: this.$t('global.Error'),
+							text: this.$t('message.emailAlreadyTaken'),
+							iconPack: 'feather',
+							icon: 'icon-alert-circle',
+							color: 'danger'
+						})
+					}
+				})
+		}
+	},
+
+  	components: {
+		vSelect,
+		flatPickr
+  	}
 }
 </script>
 
-<style lang="stylus">
-	.vs-input
-		margin 10px;
+<style>
+
 </style>
