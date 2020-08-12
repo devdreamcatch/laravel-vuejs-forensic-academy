f94dd143135fdb901718249c1fa97b5adabf38ea
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-08-12 11:15:42 -0700

    call logout function in layout

--- resources/js/src/layouts/components/navbar/components/ProfileDropDown.vue
+++ resources/js/src/layouts/components/navbar/components/ProfileDropDown.vue
@@ -63,10 +63,7 @@ export default {
   },
   methods: {
     logout () {
-      localStorage.removeItem('userInfo')
-
-      // This is just for demo Purpose. If user clicks on logout -> redirect
-      this.$router.push('/pages/login').catch(() => {})
+        this.$store.dispatch('auth/logoutJWT', {});
     }
   }
 }
