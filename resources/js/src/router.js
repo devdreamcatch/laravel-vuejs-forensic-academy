b6498c6a06620561046551be2829e81153dc0def
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-08-12 11:12:03 -0700

    auth middleware added

--- resources/js/src/router.js
+++ resources/js/src/router.js
@@ -10,6 +10,7 @@
 
 import Vue from 'vue'
 import Router from 'vue-router'
+import auth from './middleware/auth';
 
 Vue.use(Router)
 
@@ -34,11 +35,17 @@ const router = new Router({
               {
                 path: '/',
                 name: 'home',
+                meta: {
+                    middleware: auth,
+                },
                 component: () => import('./views/Home.vue')
               },
               {
                 path: '/page2',
                 name: 'page-2',
+                meta: {
+                    middleware: auth,
+                },
                 component: () => import('./views/Page2.vue')
               },
             ],
@@ -56,16 +63,19 @@ const router = new Router({
               {
                 path: '/login',
                 name: 'login',
+                meta: {},
                 component: () => import('@/views/pages/Login.vue')
               },
               {
                 path: '/register',
                 name: 'register',
+                meta: {},
                 component: () => import('@/views/pages/Register.vue')
               },
               {
                 path: '/error-404',
                 name: 'error-404',
+                meta: {},
                 component: () => import('@/views/pages/Error404.vue')
               },
             ]
@@ -80,14 +90,43 @@ const router = new Router({
 
 router.afterEach(() => {
   // Remove initial loading
-  const appLoading = document.getElementById('loading-bg')
+    const appLoading = document.getElementById('loading-bg')
     if (appLoading) {
         appLoading.style.display = "none";
     }
 })
-
+// Creates a `nextMiddleware()` function which not only
+// runs the default `next()` callback but also triggers
+// the subsequent Middleware function.
+function nextFactory(context, middleware, index) {
+    const subsequentMiddleware = middleware[index];
+    // If no subsequent Middleware exists,
+    // the default `next()` callback is returned.
+    if (!subsequentMiddleware) return context.next;
+    return (...parameters) => {
+        // Run the default Vue Router `next()` callback first.
+        context.next(...parameters);
+        // Than run the subsequent Middleware with a new
+        // `nextMiddleware()` callback.
+        const nextMiddleware = nextFactory(context, middleware, index + 1);
+        subsequentMiddleware({ ...context, next: nextMiddleware });
+    };
+}
 router.beforeEach((to, from, next) => {
-    return next()
+    if (to.meta.middleware) {
+        const middleware = Array.isArray(to.meta.middleware)
+            ? to.meta.middleware
+            : [to.meta.middleware];
+        const context = {
+            from,
+            next,
+            router,
+            to,
+        };
+        const nextMiddleware = nextFactory(context, middleware, 1);
+        return middleware[0]({ ...context, next: nextMiddleware });
+    }
+    return next();
 })
 
 export default router
