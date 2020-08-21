7a05111acb70788914fa379bd9e2522ca6711178
Devsolution <q3construction1@gmail.com>
2020-08-21 18:53:16 +0800

    remove http in personal data axios function

--- resources/js/src/store/mydata/moduleMydataActions.js
+++ resources/js/src/store/mydata/moduleMydataActions.js
@@ -1,22 +1,27 @@
 
-import jwt from '../../http/requests/mydata/jwt/index.js'
-
-import router from '@/router'
+import axios from '@/axios'
 
 export default {
-
     savePersonalData ({ commit }, payload) {
-        const { id, name, surname, cpf, sex, date_of_birth, telephone, whatsapp, email, password } = payload.userDetails
+        const { id, name, surname, cpf, sex, date_of_birth, telephone, whatsapp, email } = payload.userDetails
         return new Promise((resolve, reject) => {
-            jwt.savePersonalData(id, name, surname, cpf, sex, date_of_birth, telephone, whatsapp, email, password)
-                .then(response => {
+            axios.post(`/api/savePersonalData/${id}`, {
+                name: name,
+                surname: surname,
+                cpf: cpf,
+                sex: sex,
+                date_of_birth: date_of_birth,
+                telephone: telephone,
+                whatsapp: whatsapp,
+                email: email,
+            })
+            .then(response => {
+                // Update data in localStorage
+                commit('UPDATE_USER_INFO', response.data, {root: true})
 
-                    // Update data in localStorage
-                    commit('UPDATE_USER_INFO', response.data, {root: true})
-
-                    resolve(response)
-                })
-                .catch(error => { reject(error) })
+                resolve(response)
+            })
+            .catch(error => { reject(error) })
         })
-    },
+    }
 }
