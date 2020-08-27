31278816a874b09198649f5a677a2c32d7555bb5
kcaribou <kcaribou0428@gmail.com>
2020-08-27 09:27:11 -0700

    Change password validation fix and show notification after success operation

--- resources/js/src/views/my-data/ChangePassword.vue
+++ resources/js/src/views/my-data/ChangePassword.vue
@@ -1,20 +1,43 @@
 <template>
-  <vx-card no-shadow>
-    <vs-input class="w-full" name="old password" v-bind:label="$t('my_data.change_password.old_password')" v-model="old_password" v-validate="'required'"
-              data-vv-validate-on="blur" />
-      <span class="text-danger text-sm">{{ errors.first('old password') }}</span>
-    <vs-input class="w-full mt-base" name="new password" v-bind:label="$t('my_data.change_password.new_password')" v-model="new_password" v-validate="'required'"
-              data-vv-validate-on="blur" />
-      <span class="text-danger text-sm">{{ errors.first('new password') }}</span>
-    <vs-input class="w-full mt-base" name="confirm password" v-bind:label="$t('my_data.change_password.confirm_password')" v-model="confirm_password" v-validate="'required'"
-              data-vv-validate-on="blur" />
-      <span class="text-danger text-sm">{{ errors.first('confirm password') }}</span>
+    <vx-card no-shadow>
+        <vs-input
+            class="w-full"
+            name="old_password"
+            type="password"
+            v-bind:label="$t('my_data.change_password.old_password')"
+            v-model="old_password"
+            v-validate="'required|min:6'"
+            data-vv-validate-on="blur" />
+        <span class="text-danger text-sm">{{ errors.first('old_password') }}</span>
 
-    <!-- Save & Reset Button -->
-    <div class="flex flex-wrap items-center justify-end">
-      <vs-button class="ml-auto mt-2">{{ $t("my_data.change_password.title") }}</vs-button>
-    </div>
-  </vx-card>
+        <vs-input
+            class="w-full mt-base"
+            name="new_password"
+            type="password"
+            v-bind:label="$t('my_data.change_password.new_password')"
+            v-model="new_password"
+            v-validate="'required|min:6'"
+            data-vv-validate-on="blur" />
+        <span class="text-danger text-sm">{{ errors.first('new_password') }}</span>
+
+        <vs-input
+            class="w-full mt-base"
+            name="confirm_password"
+            type="password"
+            v-bind:label="$t('my_data.change_password.confirm_password')"
+            v-model="confirm_password"
+            v-validate="'required|min:6'"
+            data-vv-validate-on="blur" />
+        <span class="text-danger text-sm">{{ errors.first('confirm_password') }}</span>
+        <div class="flex flex-wrap items-center justify-end">
+            <vs-button
+                class="ml-auto mt-2"
+                @click="resetPassword"
+                :disabled="!validateForm">
+                {{ $t("my_data.change_password.title") }}
+            </vs-button>
+        </div>
+    </vx-card>
 </template>
 
 <script>
@@ -24,6 +47,11 @@ export default {
             old_password: '',
             new_password: '',
             confirm_password: ''
+        }
+    },
+    computed: {
+        validateForm () {
+            return !this.errors.any()
         }
     },
     methods: {
@@ -39,14 +67,14 @@ export default {
             this.$store.dispatch('mydata/resetPassword', payload)
                 .then(() => {
                     this.$vs.loading.close()
-                    // this.$vs.notify({
-                    //     title: this.$t('global.Success'),
-                    //     text: this.$t('my_data.personal_data.message_save_success'),
-                    //     iconPack: 'feather',
-                    //     icon: 'icon-check',
-                    //     position: 'top-right',
-                    //     color: 'success'
-                    // })
+                    this.$vs.notify({
+                        title: this.$t('global.Success'),
+                        text: this.$t('my_data.change_password.message_save_success'),
+                        iconPack: 'feather',
+                        icon: 'icon-check',
+                        position: 'top-right',
+                        color: 'success'
+                    })
                 })
                 .catch(error => {
                     this.$vs.loading.close()
@@ -59,6 +87,7 @@ export default {
                                 msg: error.response.data[item][0]
                             })
                         }
+                        console.log(this.errors);
                     }
                 })
         }
