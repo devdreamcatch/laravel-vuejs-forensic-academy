9451e1f8049d7f4684e115c2e02bde0b3d2b5082
kcaribou <kcaribou0428@gmail.com>
2020-08-30 20:20:09 -0700

    token issue fix

--- resources/js/src/axios.js
+++ resources/js/src/axios.js
@@ -3,14 +3,20 @@ import axios from 'axios'
 
 const baseURL = 'http://localhost:8082'
 
-export default axios.create({
-    baseURL,
-    // You can add your headers here
-    transformRequest: [function (data, headers) {
-        headers['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`
-        return data;
-    }],
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+const instance = axios.create({
+    baseURL
+});
+instance.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token;
+        }
+        config.headers['Content-Type'] = 'application/json';
+        return config;
+    },
+    error => {
+        Promise.reject(error)
+    })
+
+export default instance;
